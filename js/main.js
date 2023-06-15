9761121c06c5df9c12a7a01ffd31780b93c9dc56
Lucia <khinezarthwin21st@gmail.com>
2023-06-15 19:53:37 +0630

    New modifications are added

--- js/main.js
+++ js/main.js
@@ -11,3 +11,21 @@ function testimonialSlider() {
   }
 }
 testimonialSlider();
+
+/*---------------------------
+course preview video
+-----------------------------*/
+function coursePreviewVideo() {
+  const coursePreviewModal = document.querySelector(".js-course-preview-modal");
+  if (coursePreviewModal) {
+    //if the element is exist
+    coursePreviewModal.addEventListener("show.bs.modal", function () {
+      this.querySelector(".js-course-preview-video").play();
+      this.querySelector(".js-course-preview-video").currentTime = 0;
+    });
+    coursePreviewModal.addEventListener("hide.bs.modal", function () {
+      this.querySelector(".js-course-preview-video").pause();
+    });
+  }
+}
+coursePreviewVideo();
